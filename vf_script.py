bf789ddb7ccc4c562e7cc6ea6e46228de18e8d7e
Pasha A <pashakun@gmail.com>
2020-12-01 12:15:59 +0900

    Update vf_script.py
    Replaced Yagmail with smtplib

--- vf_script.py
+++ vf_script.py
@@ -3,19 +3,22 @@ import requests
 import numpy as np
 from datetime import date
 import pickle
+pickle.HIGHEST_PROTOCOL = 4
 import os
 import glob
 import pandas as pd
-#import smtplib, ssl
-#from email import encoders
-#from email.mime.base import MIMEBase
-#from email.mime.multipart import MIMEMultipart
-#from email.mime.text import MIMEText
-import yagmail
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = "pashakun+vf@gmail.com"
+EMAIL_PASSWORD = 'TK'
 
 timestamp = str(date.today()).replace("-","_")
 path = os.getcwd()
 
+print("Getting the scrape on for {}.".format(timestamp))
+
+# grabbing raw text from the web
 def get_text(url= "https://visionfund.com/team"):
 
     """
@@ -31,6 +34,7 @@ def get_text(url= "https://visionfund.com/team"):
     split_text = [entry.text.splitlines()[1:] for entry in team]
     return split_text
 
+# parse string from the web into a dictionary
 def dict_from_text(text, verbose=False):
 
     """
@@ -39,6 +43,8 @@ def dict_from_text(text, verbose=False):
     OUTPUT: return a dictionary with keys for names, titles and regions
 
     """
+
+    # feels safe to hardcode this
     regions = ['americas', 'asia', 'emea', "global"]
 
     # custom print function for when the verbose flag is True
@@ -52,7 +58,7 @@ def dict_from_text(text, verbose=False):
         vf_team["names"].append(entry[0])
         verboseprint("Looking at {}".format(entry[0]))
 
-        # split the remain items by comma
+        # split the remaining items by comma
         split_items = entry[1].split(',')
         verboseprint("Split items: {}".format(split_items))
 
@@ -89,11 +95,11 @@ def dict_from_text(text, verbose=False):
 
 
 
-        # check that the list isn't empty before adding
+        # check that the title list isn't empty before adding
         if inner_title_list:
             vf_team["titles"].append(inner_title_list)
 
-        # check that the list isn't empty before adding
+        # check that the region list isn't empty before adding
         if inner_region_list:
             vf_team["regions"].append(inner_region_list)
         # assignd None to bios without region
@@ -107,13 +113,21 @@ def dict_from_text(text, verbose=False):
 
 split_text = get_text()
 vf_team = dict_from_text(split_text)
+
+# save a dict copy for future refernce
+print("...Scrape complete.")
+print("...Pickling dictionaries.")
 with open(path + "/text_dicts/{}".format(timestamp), 'wb') as handle:
     pickle.dump(vf_team, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 raw_df = pd.DataFrame.from_dict(vf_team)
+print("Team member count: {}.".format(len(raw_df.index)))
+
+# save a DataFrame copy for future refernce
 raw_df.to_pickle(path + "/scrape_df/{}".format(timestamp))
+print("...Converting to a DataFrame.")
 
-
+# find the most recent  two DataFrames for comparison
 def find_old (timestamp, path):
 
     """
@@ -129,14 +143,16 @@ def find_old (timestamp, path):
 
     return (old, new)
 
+# find people who quit and those who joined
 def find_departures_additions (old_df, new_df, new_date):
 
     """
 
     INPUT: current and previous DataFrames, assumes they contain some changes
-    OUTPUT: stacked df of personelle changes
+    OUTPUT: stacked df of changes
 
     """
+    print("...Checking for personnel changes.")
 
     old_names = old_df["names"].tolist()
     new_names = new_df["names"].tolist()
@@ -144,7 +160,9 @@ def find_departures_additions (old_df, new_df, new_date):
     additions = list(set(new_names) - set(old_names))
     departures = list(set(old_names) - set(new_names))
 
+    # filter the old list for departures
     quit_df = old_df[old_df["names"].isin(departures)] if departures else pd.DataFrame()
+    # filter the new list for additions
     joined_df = new_df[new_df["names"].isin(additions)] if additions else pd.DataFrame()
 
     quit_df.set_index([["quit"]*len(quit_df)], inplace=True)
@@ -159,6 +177,7 @@ def find_departures_additions (old_df, new_df, new_date):
 
     return quit_joined_stack
 
+# does what it says on the can
 def find_titles_regions (old_df, new_df, new_date):
 
     """
@@ -168,14 +187,16 @@ def find_titles_regions (old_df, new_df, new_date):
 
     """
 
+    print("...Checking for changes in title/region.")
+
     old_names = old_df["names"].tolist()
     new_names = new_df["names"].tolist()
 
     additions = list(set(new_names) - set(old_names))
     departures = list(set(old_names) - set(new_names))
 
-    old_cleaned = old_df[~old["names"].isin(departures)] if departures else old_df
-    new_cleaned = new_df[~new["names"].isin(additions)] if additions else new_df
+    old_cleaned = old_df[~old_df["names"].isin(departures)] if departures else old_df
+    new_cleaned = new_df[~new_df["names"].isin(additions)] if additions else new_df
 
     old_cleaned = old_cleaned.rename(columns = {"titles":"old_titles", "regions":"old_regions"})
     new_cleaned = new_cleaned.rename(columns = {"titles":"new_titles", "regions":"new_regions"})
@@ -202,14 +223,18 @@ def find_titles_regions (old_df, new_df, new_date):
 
     return titles_regions_stack
 
+
+# does what is says on the can
 def create_report (old_df, new_df, new_date, path):
 
     """
 
     INPUT: current and previous DataFrames and most recent date, pwd path
-    OUTPUT: 4 html change reports -- personelle, title/region and respecitve logs
+    OUTPUT: 4 html change reports -- personnel, title/region and respecitve logs
 
     """
+
+    print("...Generating report.")
 
     report_list = []
     message = ""
@@ -219,22 +244,23 @@ def create_report (old_df, new_df, new_date, path):
 
     if not quit_joined_stack.empty:
 
-        previous_personelle_log = pd.read_pickle(path + "/change_logs/personelle_log")
-        updated_personelle_log = previous_personelle_log.append(quit_joined_stack)
-        updated_personelle_log.to_pickle(path + "/change_logs/personelle_log")
+        previous_personnel_log = pd.read_pickle(path + "/change_logs/personnel_log")
+        updated_personnel_log = previous_personnel_log.append(quit_joined_stack)
+        updated_personnel_log.to_pickle(path + "/change_logs/personnel_log")
 
-        html_personelle = quit_joined_stack.to_html()
-        html_updated_personelle_log = updated_personelle_log.to_html()
-        file_name = path + "/html_reports/personelle_changes_{}.html".format(new_date)
+        html_personnel = quit_joined_stack.to_html()
+        html_updated_personnel_log = updated_personnel_log.to_html()
+        file_name = path + "/html_reports/personnel_changes_{}.html".format(new_date)
         html_file = open(file_name, "w")
-        html_file.write(html_updated_personelle_log)
+        html_file.write(html_updated_personnel_log)
         html_file.close()
 
-        report_list.append(file_name, html_updated_personelle_log)
-        message += "Found changes in personelle.\n"
+        report_list.append(file_name)
+        report_list.append(html_updated_personnel_log)
+        message += "BINGO! Found changes in personnel.\n"
 
     else:
-        message += "Found no changes in personelle.\n"
+        message += "Found no changes in personnel.\n"
 
 
     if not titles_regions_stack.empty:
@@ -250,8 +276,9 @@ def create_report (old_df, new_df, new_date, path):
         text_file.write(html_updated_details_log)
         text_file.close()
 
-        report_list.append(file_name, html_updated_details_log)
-        message += "Found changes in title details.\n"
+        report_list.append(file_name)
+        report_list.append(html_updated_details_log)
+        message += "BINGO! Found changes in title details.\n"
 
     else:
         message += "Found no changes in title details.\n"
@@ -259,21 +286,31 @@ def create_report (old_df, new_df, new_date, path):
 
     return (message, report_list)
 
+
 old_df, new_df= find_old(timestamp, path)
 message, report_list = create_report(old_df, new_df, timestamp, path)
 attachment_list = [item for item in report_list if item]
 
-receiver = "pashakun+vf@gmail.com"
-
-yag = yagmail.SMTP("pashakun@gmail.com", oauth2_file="~/secret.json")
-yag.send(
-    to=receiver,
-    subject="Your Vision Fund scrape results are here.",
-    contents=message,
-    attachments=attachment_list
-)
-
+print("...Saving logs.")
 file_name = path + "/run_logs/log_{}.html".format(timestamp)
 text_file = open(file_name, "w")
 text_file.write(message)
 text_file.close()
+
+print("...Sending a message.")
+msg = EmailMessage()
+msg['Subject'] = 'Your Vision Fund scrape results are here.'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
+msg.set_content(message)
+
+if len(attachment_list) > 0:
+    for file_name in attachment_list:
+        with open(file_name, 'rb') as html:
+            msg.add_attachment(html.read(), maintype='application', subtype='octet-stream', filename=html.name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
+
+print("Scrape complete. Check your email for results.")
